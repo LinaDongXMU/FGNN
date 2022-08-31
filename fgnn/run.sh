cd6b48f1820f629f3c313ad148b946a7e0dc632b
LinaDongXMU <90234051+LinaDongXMU@users.noreply.github.com>
2022-08-31 16:02:00 +0800

    Update run.sh

--- fgnn/run.sh
+++ fgnn/run.sh
@@ -1,2 +1,3 @@
+mkdir results
 python prepare_files.py
 python preprocess_pdbbind.py --data_path_core pdbbind2016/coreset --data_path_refined pdbbind2016/refined-set --dataset_name pdbbind2016 --output_path results --cutoff 5.5
