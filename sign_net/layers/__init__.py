0c43fd09f6187c27fc9130cfe6c3f516a01ecb40
shishuai <shishaui@qutronix.com>
2022-09-28 15:32:52 +0800

    transformer with edge and without edge

--- sign_net/layers/__init__.py
+++ sign_net/layers/__init__.py
@@ -3,3 +3,4 @@ from .signnet import SignNetLayer
 from .model_utils import RegressionLayer, MaskedBN
 from .gine import GINE
 from .graphtransformer import MultiHeadAttentionLayer, GraphTransformerLayer
+from .graphtransformer_edge import MultiHeadAttentionLayer_edge, GraphTransformerLayer_edge
--- sign_net/layers/graphtransformer.py
+++ sign_net/layers/graphtransformer.py
@@ -7,42 +7,38 @@ from torch_scatter import scatter
 
 
 class MultiHeadAttentionLayer(MessagePassing):
-    def __init__(self, in_dim, out_dim, num_heads, using_bias=False, update_edge_feats=True):
+    def __init__(self, in_dim, out_dim, num_heads, using_bias=True):
         super().__init__()
         self.out_dim = out_dim
         self.num_heads = num_heads
         self.using_bias = using_bias
-        self.update_edge_feats = update_edge_feats
         
         self.Q = nn.Linear(in_dim, out_dim*num_heads, bias=using_bias)
         self.K = nn.Linear(in_dim, out_dim*num_heads, bias=using_bias)
         self.V = nn.Linear(in_dim, out_dim*num_heads, bias=using_bias)
-        self.edge_feats_projection = nn.Linear(in_dim, out_dim*num_heads, bias=using_bias)
         
-    def forward(self, node_feats, edge_feats, edge_index):
-        v, z, edge_feat = self.propagate(edge_index=edge_index, x=node_feats, edge_feats=edge_feats)
+    def forward(self, node_feats, edge_index):
+        v, z = self.propagate(edge_index=edge_index, x=node_feats)
         
         node_feats = v / z + torch.full_like(z, 1e-6)
-        return node_feats, edge_feat
+        return node_feats
         
-    def message(self, x_i, x_j, edge_feats):
+    def message(self, x_i, x_j):
         q_i = self.Q(x_i).view(-1, self.num_heads, self.out_dim)
         k_j = self.K(x_j).view(-1, self.num_heads, self.out_dim)
         v_j = self.V(x_j).view(-1, self.num_heads, self.out_dim)
         
-        edge_feat_projection = self.edge_feats_projection(edge_feats).view(-1, self.num_heads, self.out_dim)
-        att_score = ((q_i * k_j) / np.sqrt(self.out_dim)).clamp(-5.0,5.0)
-        edge_feats = torch.exp(att_score * edge_feat_projection).clamp(-5.0,5.0)
+        att_score = (q_i * k_j).sum(-1, keepdim=True)
+        att_score = torch.exp(att_score / np.sqrt(self.out_dim)).clamp(-5.0,5.0)
         
-        msg = edge_feats * v_j
-        return msg, edge_feats
+        msg = v_j * att_score
+        return msg, att_score
         
     def aggregate(self, inputs, index, ptr=None, dim_size=None):
-        msg, edge_feats = inputs
+        msg, att_score = inputs
         
-        return [scatter(msg, index, dim=-3, dim_size=dim_size, reduce=self.aggr),
-                scatter(edge_feats, index, dim=-3, dim_size=dim_size, reduce=self.aggr),
-                edge_feats]
+        return [scatter(msg, index, dim=0, dim_size=dim_size, reduce=self.aggr),
+                scatter(att_score, index, dim=0, dim_size=dim_size, reduce=self.aggr)]
 
     def update(self, agg_out):
         return agg_out
@@ -57,94 +53,67 @@ class GraphTransformerLayer(nn.Module):
         self.num_heads = num_heads
         self.dropout = dropout
         self.residual = residual
-        self.layer_norm = layer_norm     
+        self.layer_norm = layer_norm        
         self.batch_norm = batch_norm
         
         self.attention = MultiHeadAttentionLayer(in_dim, out_dim//num_heads, num_heads, use_bias)
         
-        self.O_h = nn.Linear(out_dim, out_dim)
-        self.O_e = nn.Linear(out_dim, out_dim)
+        self.O = nn.Linear(out_dim, out_dim)
 
         if self.layer_norm:
-            self.layer_norm1_h = nn.LayerNorm(out_dim)
-            self.layer_norm1_e = nn.LayerNorm(out_dim)
+            self.layer_norm1 = nn.LayerNorm(out_dim)
             
         if self.batch_norm:
-            self.batch_norm1_h = nn.BatchNorm1d(out_dim)
-            self.batch_norm1_e = nn.BatchNorm1d(out_dim)
+            self.batch_norm1 = nn.BatchNorm1d(out_dim)
         
-        # FFN for h
-        self.FFN_h_layer1 = nn.Linear(out_dim, out_dim*2)
-        self.FFN_h_layer2 = nn.Linear(out_dim*2, out_dim)
-        
-        # FFN for e
-        self.FFN_e_layer1 = nn.Linear(out_dim, out_dim*2)
-        self.FFN_e_layer2 = nn.Linear(out_dim*2, out_dim)
+        # FFN
+        self.FFN_layer1 = nn.Linear(out_dim, out_dim*2)
+        self.FFN_layer2 = nn.Linear(out_dim*2, out_dim)
 
         if self.layer_norm:
-            self.layer_norm2_h = nn.LayerNorm(out_dim)
-            self.layer_norm2_e = nn.LayerNorm(out_dim)
+            self.layer_norm2 = nn.LayerNorm(out_dim)
             
         if self.batch_norm:
-            self.batch_norm2_h = nn.BatchNorm1d(out_dim)
-            self.batch_norm2_e = nn.BatchNorm1d(out_dim)
+            self.batch_norm2 = nn.BatchNorm1d(out_dim)
         
-    def forward(self, node_feat, edge_feat, edge_index):
+    def forward(self, node_feat, edge_index):
         h_in1 = node_feat # for first residual connection
-        e_in1 = edge_feat # for first residual connection
         
         # multi-head attention out
-        h_attn_out, e_attn_out = self.attention(node_feat, edge_feat, edge_index)
-        
-        h = h_attn_out.view(-1, self.out_channels)
-        e = e_attn_out.view(-1, self.out_channels)
+        attn_out = self.attention(node_feat, edge_index)
+        h = attn_out.view(-1, self.out_channels)
         
         h = F.dropout(h, self.dropout, training=self.training)
-        e = F.dropout(e, self.dropout, training=self.training)
-
-        h = self.O_h(h)
-        e = self.O_e(e)
-
+        
+        h = self.O(h)
+        
         if self.residual:
             h = h_in1 + h # residual connection
-            e = e_in1 + e # residual connection
-
+        
         if self.layer_norm:
-            h = self.layer_norm1_h(h)
-            e = self.layer_norm1_e(e)
-
+            h = self.layer_norm1(h)
+            
         if self.batch_norm:
-            h = self.batch_norm1_h(h)
-            e = self.batch_norm1_e(e)
-
+            h = self.batch_norm1(h)
+        
         h_in2 = h # for second residual connection
-        e_in2 = e # for second residual connection
-
-        # FFN for h
-        h = self.FFN_h_layer1(h)
+        
+        # FFN
+        h = self.FFN_layer1(h)
         h = F.relu(h)
         h = F.dropout(h, self.dropout, training=self.training)
-        h = self.FFN_h_layer2(h)
-
-        # FFN for e
-        e = self.FFN_e_layer1(e)
-        e = F.relu(e)
-        e = F.dropout(e, self.dropout, training=self.training)
-        e = self.FFN_e_layer2(e)
+        h = self.FFN_layer2(h)
 
         if self.residual:
-            h = h_in2 + h # residual connection       
-            e = e_in2 + e # residual connection  
-
+            h = h_in2 + h # residual connection
+        
         if self.layer_norm:
-            h = self.layer_norm2_h(h)
-            e = self.layer_norm2_e(e)
-
+            h = self.layer_norm2(h)
+            
         if self.batch_norm:
-            h = self.batch_norm2_h(h)
-            e = self.batch_norm2_e(e)             
+            h = self.batch_norm2(h)       
 
-        return h, e
+        return h
         
     def __repr__(self):
         return '{}(in_channels={}, out_channels={}, heads={}, residual={})'.format(self.__class__.__name__,
@@ -157,7 +126,7 @@ if __name__ == "__main__":
     edge_index = torch.randint(0, 16, (2, 96))
     edge_feat = torch.randn((96, 64))
 
-    model = GraphTransformerLayer(64, 64, 8, 0.1, True)
-    out = model(node, edge_feat, edge_index)
-    print(f'node: {out[0].shape}')
-    print(f'edge: {out[1].shape}')
+    model = GraphTransformerLayer(64, 64, 8, dropout=0.1, layer_norm=True, use_bias=True)
+    out = model(node, edge_index)
+    print(f'node: {out.shape}')
+    # print(f'edge: {out[1].shape}')
