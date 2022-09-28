f78d4a7ec841c053efb5e048432727f214a6a257
shishuai <shishaui@qutronix.com>
2022-09-28 14:42:33 +0800

    add graphtransformerlayer

--- sign_net/layers/__init__.py
+++ sign_net/layers/__init__.py
@@ -2,3 +2,4 @@ from .egnn import EGNNLayer
 from .signnet import SignNetLayer
 from .model_utils import RegressionLayer, MaskedBN
 from .gine import GINE
+from .graphtransformer import MultiHeadAttentionLayer, GraphTransformerLayer
