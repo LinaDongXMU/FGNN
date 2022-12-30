959cb564c4b435d907e25029cf1272b9fa6c3603
LinaDongXMU <90234051+LinaDongXMU@users.noreply.github.com>
2022-12-30 15:33:43 +0800

    Delete sign_net directory

--- sign_net/layers/__init__.py
+++
@@ -1,6 +0,0 @@
-from .egnn import EGNNLayer
-from .signnet import SignNetLayer, SignNetLayer_Transformer, SignNetLayer_Transformer2
-from .model_utils import RegressionLayer, MaskedBN
-from .gine import GINE
-from .graphtransformer import MultiHeadAttentionLayer, MultiHeadAttentionLayer_2, GraphTransformerLayer
-from .graphtransformer_edge import MultiHeadAttentionLayer_edge, MultiHeadAttentionLayer_edge_2, GraphTransformerLayer_edge
