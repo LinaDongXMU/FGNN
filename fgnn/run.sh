b9b09b94373833fae56c2b62d54cee52f199edc0
LinaDongXMU <90234051+LinaDongXMU@users.noreply.github.com>
2022-12-30 15:34:06 +0800

    Delete fgnn directory

--- fgnn/run.sh
+++
@@ -1,6 +0,0 @@
-mkdir results
-mkdir data
-mkdir data/cache
-mkdir data/data_cache
-python prepare_files.py
-python preprocess_pdbbind.py --data_path_core pdbbind2016/coreset --data_path_refined pdbbind2016/refined-set --dataset_name pdbbind2016 --output_path results --cutoff 5.5
